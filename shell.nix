41c3836c479bcb97be986d24c1c4fe03d36fccc2
ilian <ilian@tuta.io>
2021-01-06 00:23:51 +0100

    Allow bootstrapping from install image

---
+++ shell.nix
@@ -0,0 +1,35 @@
+# Use shell-nix to use nix command with flakes support
+# This is useful when bootstrapping from a NixOS install image
+
+let
+  unstable = builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz;
+  pkgs = import unstable { };
+  installPkgs = (import "${unstable}/nixos" {
+    configuration = {};
+    system = pkgs.system;
+  }).config.system.build;
+in
+
+pkgs.stdenvNoCC.mkDerivation {
+  name = "flakes-bootstrap";
+  nativeBuildInputs = with installPkgs; [
+    pkgs.nixFlakes
+
+    # Override potentially old scripts of install image
+    nixos-generate-config
+    nixos-install
+    nixos-enter
+    nixos-rebuild
+    # manual.manpages
+  ];
+  
+  shellHook = ''
+    # Work around error of nixos-rebuild: unrecognised flag '--experimental-features'
+    # Let nixos-rebuild use the nix package we define
+    export _NIXOS_REBUILD_REEXEC=1 
+
+    nix() {
+      ${pkgs.nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
+    }
+  '';
+}
