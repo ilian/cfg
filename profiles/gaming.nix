ff737459babdebca36d8c51cdf101fa826fcdc78
ilian <ilian@tuta.io>
2021-02-19 19:01:41 +0100

    Add port forwarding for steam

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   hardware.opengl.driSupport32Bit = true;
@@ -7,4 +7,18 @@
     steam
     steam-run
   ];
+
+  networking.firewall = lib.mkMerge [
+    {
+      # Remote play
+      allowedTCPPorts = [ 27036 ];
+      allowedUDPPortRanges = [ { from = 27031; to = 27036; } ];
+    }
+    {
+      # Dedicated servers
+      allowedTCPPorts = [ 27015 ]; # SRCDS Rcon port
+      allowedUDPPorts = [ 27015 ]; # Gameplay traffic
+    }
+  ];
+
 }
