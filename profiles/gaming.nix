98595f9be7f8e108c9fabb31b68ba6b6c69a7326
ilian <ilian@tuta.io>
2021-02-22 12:51:44 +0100

    Use steam module to open firewall

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,24 +1,12 @@
 { pkgs, lib, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
+
   environment.systemPackages = with pkgs; [
-    steam
     steam-run
   ];
-
-  networking.firewall = lib.mkMerge [
-    {
-      # Remote play
-      allowedTCPPorts = [ 27036 ];
-      allowedUDPPortRanges = [ { from = 27031; to = 27036; } ];
-    }
-    {
-      # Dedicated servers
-      allowedTCPPorts = [ 27015 ]; # SRCDS Rcon port
-      allowedUDPPorts = [ 27015 ]; # Gameplay traffic
-    }
-  ];
-
 }
