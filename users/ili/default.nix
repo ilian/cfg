bae9a9741890846a2b8cbf1efbf923c1b9913447
ilian <ilian@tuta.io>
2021-04-15 10:43:35 +0200

    Add authorized keys

--- users/ili/default.nix
+++ users/ili/default.nix
@@ -10,6 +10,7 @@ with lib;
       ++ optionals config.networking.networkmanager.enable [ "networkmanager" ]
       ++ optionals config.virtualisation.docker.enable [ "docker" ]
       ++ optionals config.services.jack.jackd.enable [ "jackaudio" ];
+    openssh.authorizedKeys.keyFiles = [ ./ssh-keys/apple.pub ];
   };
 
   home-manager.users.ili = {
