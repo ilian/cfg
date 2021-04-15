3809d9bac8475840c801bad621f1606a522d7614
ilian <ilian@tuta.io>
2021-04-15 10:43:03 +0200

    mai: remove cloud-init

--- hosts/mai/configuration.nix
+++ hosts/mai/configuration.nix
@@ -6,22 +6,10 @@
     ../../users/ili
   ];
 
-  services.sshd.enable = true;
-
   boot.loader.grub.device = "/dev/sda";
   boot.loader.timeout = 0;
 
-  services.cloud-init.enable = true;
-  services.cloud-init.config = options.services.cloud-init.config.default + ''
-    unverified_modules:
-     - ssh-import-id
-     - ca-certsp
-  '';
-
-  # Work around https://bugs.launchpad.net/cloud-init/+bug/1404060
-  services.openssh.extraConfig = ''
-    AuthorizedKeysFile .ssh/authorized_keys
-  '';
+  services.sshd.enable = true;
 
   networking.hostName = "mai";
 }
