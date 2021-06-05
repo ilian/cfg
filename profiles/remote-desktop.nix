942b2338b065b44c537a421f9ebb9510d2061309
ilian <ilian@tuta.io>
2021-06-05 16:38:25 +0200

    remote-desktop: use xrdp

--- profiles/remote-desktop.nix
+++ profiles/remote-desktop.nix
@@ -1,11 +1,8 @@
 {
-  services.xserver = {
-    enable = true;
-    desktopManager = {
-      xterm.enable = false;
-      xfce.enable = true;
-    };
-    displayManager.defaultSession = "xfce";
-  };
-  programs.x2goserver.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "startplasma-x11";
 }
