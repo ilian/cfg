57698781112de4c21999627eeddf1a91e433ee84
ilian <ilian@tuta.io>
2021-07-16 15:50:11 +0200

    Disable treesitter

--- users/ili/dev/vim/default.nix
+++ users/ili/dev/vim/default.nix
@@ -96,16 +96,16 @@ in {
       :luafile ${luaDir}/init.lua
 
 
-      lua <<EOF
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        ignore_install = {}, -- List of parsers to ignore installing
-        highlight = {
-          enable = true,              -- false will disable the whole extension
-          disable = {},  -- list of language that will be disabled
-        },
-      }
-      EOF
+      " lua <<EOF
+      " require'nvim-treesitter.configs'.setup {
+      "   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+      "   ignore_install = {}, -- List of parsers to ignore installing
+      "   highlight = {
+      "     enable = true,              -- false will disable the whole extension
+      "     disable = {},  -- list of language that will be disabled
+      "   },
+      " }
+      " EOF
 
       if has('termguicolors')
         set termguicolors
