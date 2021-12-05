281257b5a33bcc7cd3f4a2d6ee035a644c30acb8
ilian <ilian@tuta.io>
2021-12-05 14:01:17 +0100

    kde: enable numlock on boot

--- profiles/kde.nix
+++ profiles/kde.nix
@@ -2,6 +2,7 @@
 
 {
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.autoNumlock = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   environment.systemPackages = with pkgs; with plasma5Packages; [
