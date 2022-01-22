aa8a45145e64aa57212128245bb167e5d131fb79
ilian <ilian@tuta.io>
2022-01-22 22:11:18 +0100

    Report battery status of bluetooth devices

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -1,4 +1,11 @@
 {
   hardware.bluetooth.enable = true;
+  # Report bluetooth headphone battery level
+  hardware.bluetooth.settings = {
+    General = {
+      Experimental = "true";
+    };
+  };
+
   networking.networkmanager.enable = true;
 }
