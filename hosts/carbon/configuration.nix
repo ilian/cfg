36ad7fbbd4cd564263f12bd262519b839c6baeef
ilian <ilian@tuta.io>
2022-01-23 13:09:47 +0100

    carbon: mask EC interrupts to improve low-latency audio

--- hosts/carbon/configuration.nix
+++ hosts/carbon/configuration.nix
@@ -17,6 +17,10 @@
 
   networking.hostName = "carbon";
 
+  # Ignore EC interrupts that cause low-latency audio issues
+  # https://gist.github.com/ilian/439be7f83db31d58ce448eeb9ed34323
+  boot.kernelParams = [ "acpi_mask_gpe=0x6e" ];
+
   services.tlp = {
     enable = true;
     settings = {
