2de0ba1b073f95db479f00d5fb6f6951823e5db3
ilian <ilian@tuta.io>
2023-03-31 14:03:51 +0200

    Configure Helix

--- users/ili/dev/helix.nix
+++ users/ili/dev/helix.nix
@@ -1,16 +1,38 @@
 { pkgs, ... }:
+
+let
+
+  theme="tokyonight_storm";
+  lf-pick = pkgs.writeShellScriptBin "lf-pick" ''
+    function lfp(){
+      local TEMP=$(mktemp)
+      ${pkgs.lf}/bin/lf -selection-path="$TEMP"
+      cat "$TEMP"
+      rm "$TEMP"
+    }
+
+    lfp
+  '';
+
+in
+
 {
   home.packages = with pkgs; [
     jsonnet-language-server
     nil # nix language server
     nodePackages.bash-language-server
+    nodePackages.vscode-json-languageserver
     terraform-ls
     yaml-language-server
   ];
   programs.helix = {
     enable = true;
     settings = {
-      theme = "base16_transparent";
+      theme = theme;
+      editor = {
+        bufferline = "always";
+        rulers = [80];
+      };
       editor.cursor-shape = {
         insert = "bar"; # Change cursor to a bar instead of a block
       };
@@ -18,9 +40,25 @@
         display-messages = true;
       };
       keys.normal = {
+        "0" = "goto_line_start";
+        "^" = "goto_first_nonwhitespace";
+        "$" = "goto_line_end";
         G = "goto_file_end";
         ret = ["move_line_down" "goto_first_nonwhitespace"];
         ";" = "command_mode";
+        # Tree file picker, based on https://gist.github.com/lukepighetti/8e4a13db5bdcd68a7d83eee19051ab14
+        # Broken due to broken UI after picking a file
+        # "C-f" = [
+        #   ":new"
+        #   ":theme default"
+        #   ":insert-output ${lf-pick}/bin/lf-pick"
+        #   "select_all"
+        #   "split_selection_on_newline"
+        #   "goto_file"
+        #   "goto_last_modified_file"
+        #   ":buffer-close!"
+        #   ":theme ${theme}"
+        # ];
       };
       keys.normal.g = {
         q = ":reflow";
@@ -28,6 +66,11 @@
       keys.normal.space = {
         q = ":quit";
         w = ":write";
+      };
+      keys.select = {
+        "0" = "goto_line_start";
+        "^" = "goto_first_nonwhitespace";
+        "$" = "goto_line_end";
       };
     };
   };
