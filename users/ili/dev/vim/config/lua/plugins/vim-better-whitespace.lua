9f1805c34bc054e5c23972f5139452b8175cc3fb
ilian <ilian@tuta.io>
2023-06-19 23:07:07 +0200

    vim: turn off whitespace highlighting as it was visible in I mode

--- users/ili/dev/vim/config/lua/plugins/vim-better-whitespace.lua
+++ users/ili/dev/vim/config/lua/plugins/vim-better-whitespace.lua
@@ -2,7 +2,7 @@ return {
   'ntpeters/vim-better-whitespace',
 
   init = function()
-    vim.g.better_whitespace_enabled = 1
+    vim.g.better_whitespace_enabled = 0
     vim.g.strip_whitespace_on_save = 1
     vim.g.strip_whitespace_confirm = 1
     vim.g.better_whitespace_operator = '_s'
