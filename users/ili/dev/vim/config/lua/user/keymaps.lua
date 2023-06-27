d5746819e82c9059988fbc4443eec60a914cd543
ilian <ilian@tuta.io>
2023-06-27 19:46:22 +0200

    vim: keymap to move across windows

--- users/ili/dev/vim/config/lua/user/keymaps.lua
+++ users/ili/dev/vim/config/lua/user/keymaps.lua
@@ -36,3 +36,9 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
 
+-- Navigating windows without <C-w>
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
