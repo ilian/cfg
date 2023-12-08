79f3202c73841e20fb63d4b35da08be7bfdf8911
ilian <ilian@tuta.io>
2023-12-08 20:17:43 +0100

    helix: change theme, more keybinds, remove deprecated lang config

--- users/ili/dev/helix.nix
+++ users/ili/dev/helix.nix
@@ -9,16 +9,24 @@
     nodePackages.bash-language-server
     terraform-ls
     yaml-language-server
+    #rust-analyzer
+    lf
   ];
+
+  home.sessionVariables.EDITOR = "hx";
+
   programs.helix = {
     enable = true;
+    package = pkgs.unstable.helix;
     settings = {
-      theme = "tokyonight_storm";
+      theme = "kanagawa";
       editor = {
         cursor-shape.insert = "bar";
-        lsp.display-messages = true;
-        indent-guides.render = true;
         idle-timeout = 200;
+        indent-guides.render = true;
+        line-number = "relative";
+        lsp.display-inlay-hints = true;
+        lsp.display-messages = true;
         rulers = [80];
         statusline = {
           left = ["mode" "selections" "spinner" "file-name" "total-line-numbers"];
@@ -39,12 +47,13 @@
           common
           {
             V = ["select_mode" "extend_to_line_bounds"];
-            ";" = "command_mode";
+            D = ["ensure_selections_forward" "extend_to_line_end" "delete_selection"];
 
             g.q = ":reflow";
             space.q = ":quit";
             space.w = ":write";
             space.space = "goto_last_accessed_file";
+            space.t = [":new" ":insert-output lf -selection-path=/dev/stdout" "split_selection_on_newline" "goto_file" "goto_last_modification" "goto_last_modified_file" ":buffer-close!" ":redraw"];
           }
         ];
         select = lib.mkMerge [
@@ -53,20 +62,5 @@
         ];
       };
     };
-    languages = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.alejandra}/bin/alejandra";
-        formatter.args = ["-"];
-        language-server.command = "${pkgs.nil}/bin/nil";
-      }
-      {
-        name = "json";
-        auto-format = true;
-        language-server.command = "${pkgs.nodePackages.vscode-json-languageserver}/bin/vscode-json-languageserver";
-        language-server.args = ["--stdio"];
-      }
-    ];
   };
 }
