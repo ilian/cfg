506de77992ce6e9288c3148e1d84da2e343c8f0c
ilian <ilian@tuta.io>
2025-06-15 13:14:55 +0200

    vscode: set files.trimTrailingWhitespace to true

--- users/ili/dev/vim/config/lua/plugins/vim-better-whitespace.lua
+++ users/ili/dev/vim/config/lua/plugins/vim-better-whitespace.lua
@@ -6,6 +6,10 @@ return {
 		vim.g.better_whitespace_enabled = 0
 		if vim.g.vscode then
 			-- Built-in
+			-- Trim Auto Whitespace is broken when using VSCode
+			-- Trim Trailing Whitespace (on save) is a good alternative
+			local vscode = require("vscode")
+			vscode.update_config("files.trimTrailingWhitespace", true, "global")
 			vim.g.strip_whitespace_on_save = 0
 			vim.g.strip_whitespace_confirm = 0
 		else
