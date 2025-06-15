5dc5e3714178207b3b8c2e5367245c5a87148707
ilian <ilian@tuta.io>
2025-06-15 11:54:54 +0200

    Use uxplay from unstable

--- profiles/airplay-receiver.nix
+++ profiles/airplay-receiver.nix
@@ -3,9 +3,9 @@
 
   # uxplay -p needed to use the ports below instead of random ones
   networking.firewall.allowedUDPPorts = [ 6000 6001 7011 ];
-  networking.firewall.allowedTCPPorts = [ 7000 7100 ];
+  networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
 
   environment.systemPackages = with pkgs; [
-    uxplay
+    unstable.uxplay
   ];
 }
