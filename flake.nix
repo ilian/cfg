e88c03304caea4020ecd6c294932ac0c604ba736
ilian <ilian@tuta.io>
2025-06-15 11:53:08 +0200

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils/flatten-tree-system";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
---
+++ profiles/airplay-receiver.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+
+  # uxplay -p needed to use the ports below instead of random ones
+  networking.firewall.allowedUDPPorts = [ 6000 6001 7011 ];
+  networking.firewall.allowedTCPPorts = [ 7000 7100 ];
+
+  environment.systemPackages = with pkgs; [
+    uxplay
+  ];
+}
