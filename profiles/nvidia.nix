1cd8cf69776b3fab8a51856aad997138371957a3
ilian <ilian@tuta.io>
2025-06-15 12:00:23 +0200

    Add hardware.nvidia options

--- profiles/nvidia.nix
+++ profiles/nvidia.nix
@@ -1,3 +1,18 @@
+{ config, ...}:
 {
+
+  #hardware.graphics = {
+  #  enable = true;
+  #};
+
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
