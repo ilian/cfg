c988e18bdd111af7991f1f548a783c558edabcc9
ilian <ilian@tuta.io>
2025-06-15 11:59:51 +0200

    Add libimobiledevice, ifuse

--- profiles/idevice.nix
+++ profiles/idevice.nix
@@ -1,7 +1,14 @@
+{ pkgs, ... }:
+
 {
   services.usbmuxd.enable = true;
   # DFU mode
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="122[27]|128[0-3]", TAG+="uaccess", TAG+="udev-acl"
   '';
+
+  environment.systemPackages = with pkgs; [
+    libimobiledevice # idevicesyslog
+    ifuse # copying files never has been this easy!
+  ];
 }
