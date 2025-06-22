8c76d65a501eafe2e139fc84be0e0e420c613bf1
ilian <ilian@tuta.io>
2025-06-22 17:42:33 +0200

    Manage ili user with nix-darwin

--- users/ili/darwin/default.nix
+++ users/ili/darwin/default.nix
@@ -1,15 +1,22 @@
-{ pkgs, home-manager, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ../common
   ];
 
-  users.users.ili = {
-    name = "ili";
-    createHome = true;
-    home = "/Users/ili";
-    isHidden = false;
-    shell = pkgs.bashInteractive;
+  # Users are expected to be created in System Preferences and assigned admin group
+  users = {
+    knownUsers = [ "ili" ];
+    users = {
+      ili = {
+        name = "ili";
+        createHome = true;
+        home = "/Users/ili";
+        isHidden = false;
+        shell = pkgs.bashInteractive;
+        uid = 510;
+      };
+    };
   };
 }
