bcacf1ca660a7d30399e3096da6b1df4994d8f97
ilian <ilian@tuta.io>
2025-07-16 11:44:44 +0200

    darwin: refresh maven creds with cronjob

--- users/ili/darwin/dev/default.nix
+++ users/ili/darwin/dev/default.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
 {
 
   programs.alacritty.settings.font.size = 18;
@@ -9,4 +10,26 @@
     docker
     docker-compose
   ];
+
+  launchd.agents = {
+    codeartifact-maven-updater = {
+      enable = true;
+      config =
+        let
+          homeDirectory = config.home.homeDirectory;
+          tokenValiditySeconds = 12 * 60 * 60;
+        in
+        {
+          ProgramArguments = [
+            "${homeDirectory}/bin/codeartifact-maven-updater"
+            "-f" # Force token generation even if token is still valid
+            "-d"
+            (toString tokenValiditySeconds)
+          ];
+          StartInterval = tokenValiditySeconds - 60;
+          StandardOutPath = "${homeDirectory}/Library/Logs/codeartifact-maven-updater/out.log";
+          StandardErrorPath = "${homeDirectory}/Library/Logs/codeartifact-maven-updater/err.log";
+        };
+    };
+  };
 }
