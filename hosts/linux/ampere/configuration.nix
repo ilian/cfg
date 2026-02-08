98300dcb646e43858ad0283aebea2a5192e5411c
ilian <git@ilian.dev>
2026-02-08 16:03:47 +0100

    ampere: add k3s with cilium

--- hosts/linux/ampere/configuration.nix
+++ hosts/linux/ampere/configuration.nix
@@ -17,8 +17,40 @@
   };
 
   nix.settings.trusted-users = [ "root" "@wheel" ];
+
   programs.mosh.enable = true;
   services.tailscale.enable = true;
+
+  # Install cilium after creating a k3s cluster:
+  # cilium install --version 1.19.0 --set=ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16"
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = [
+     "--tls-san 100.80.57.27"
+     # Cilium
+     "--disable=traefik,flannel,servicelb"
+     "--flannel-backend=none"
+     "--disable-network-policy"
+     "--disable-kube-proxy"
+     "--disable-cloud-controller"
+    ];
+  };
+
+  networking.firewall = {
+    enable = true;
+
+    extraCommands = ''
+      iptables -A INPUT -s 10.42.0.0/16 -j ACCEPT # Cilium
+    '';
+
+    checkReversePath = false; # Cilium
+  };
+
+  environment.systemPackages = with pkgs; [
+    kubectl
+    cilium-cli
+  ];
 
   services.openssh.enable = true;
   services.openssh.forwardX11 = true;
